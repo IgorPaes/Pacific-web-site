b3bf53537172d667f1c8122947e578110c38c15f
Igor <igor.paes88@gmail.com>
2023-11-30 19:16:39 -0300

    Inicio na implementação da navegação.

---
+++ geral/js/navTopo.js
@@ -0,0 +1,17 @@
+function navegacao(selecionado) {
+    const itensNav = document.querySelectorAll('.top_nav button');
+    switch(selecionado) {
+        case 1:
+            itensNav[0].classList.add('selecionado');
+        break;
+        case 2:
+            itensNav[1].classList.add('selecionado');
+        break;
+        case 3:
+            itensNav[2].classList.add('selecionado');
+        break;
+    }
+    `
+        123
+    `
+}
