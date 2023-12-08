143e9f9954876618a42270f3e32b46083e2e7326
Igor <igor.paes88@gmail.com>
2023-12-08 13:13:55 -0300

    Primeiros passos na implementação das categorias de produtos

--- produtos/produtos.js
+++ produtos/produtos.js
@@ -1,2 +1,5 @@
 import navegacoes from '../geral/js/navegacoes.js';
 navegacoes(2);
+
+const blocoMain = document.querySelector('.main_container');
+const btnsVerMais = document.querySelectorAll('.item_block .learn_more_product')
